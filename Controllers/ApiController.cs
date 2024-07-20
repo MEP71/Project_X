a5af68676792b49b3a7a4c4ec6bd7a37267fb4ce
Maksymilian Edward Piotrowski <me.pio@outlook.com>
2024-07-20 13:27:11 +0200

    Add project files.

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_X.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "Hello from API!" });
+        }
+    }
+}
---
+++ Pages/Example.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Project_X.Pages
+{
+    public class ExampleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
